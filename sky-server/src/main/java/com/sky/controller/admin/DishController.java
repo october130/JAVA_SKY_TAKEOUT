a20c3a934dac0a17f6081397aa2a53d47700791d
software <Hello@software.cn>
2026-05-28 15:50:04 +0800

    增加redis缓存菜品，效率优化，不必在数据库中大量查询 引用springcache用来处理新增，删除，根据分类id查询时顺带删除缓存中数据的操作

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -13,9 +13,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -24,7 +26,8 @@ import java.util.List;
 public class DishController {
     @Autowired
     private Dishservice dishservice;
-
+@Autowired
+    private RedisTemplate redisTemplate;
 
 
 
@@ -35,6 +38,9 @@ public class DishController {
         //当然，在新增菜品中图像上传的功能已经在common包中已经实现，所以这里就不在重复实现了
         log.info("新增菜品 :{}", dishDTO);
        dishservice.saveWithFlavor(dishDTO);//dishDTO对象中包含了菜品数据，以及菜品对应的口味数据
+
+        String key = "dish_" + dishDTO.getCategoryId();
+        redisTemplate.delete(key);
 
         return Result.success();
     }
@@ -54,6 +60,10 @@ public class DishController {
     public Result delete(@RequestParam List<Long>ids){
         log.info("删除菜品：{}", ids);
         dishservice.delete(ids);//根据前端放的id进行批量删除
+
+        Set keys = redisTemplate.keys("dish_*");
+        redisTemplate.delete(keys);
+
         return Result.success();
 
     }
@@ -71,6 +81,9 @@ public class DishController {
 public Result update(@RequestBody DishDTO dishDTO){
         log.info("修改菜品：{}", dishDTO);
         dishservice.updateflavor(dishDTO);
+
+    Set keys = redisTemplate.keys("dish_*");
+    redisTemplate.delete(keys);
         return Result.success();
 
 }
--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -11,6 +11,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,6 +35,7 @@ public class SetmealController {
 
     @PostMapping
     @ApiOperation("新增套餐操作")
+    @CacheEvict(value = "setmealCache",key = "#setmealDTO.categoryId")
     public Result save( @RequestBody SetmealDTO setmealDTO ){
         log.info("新增套餐：{}", setmealDTO);
         setmealService.saveWithDish(setmealDTO);
@@ -42,6 +44,7 @@ public class SetmealController {
 
     @DeleteMapping
     @ApiOperation("批量删除套餐操作")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result delete(@RequestParam List<Long> ids){
         log.info("批量删除套餐：{}", ids);
         setmealService.delete(ids);
@@ -59,6 +62,7 @@ public class SetmealController {
 
     @PutMapping
     @ApiOperation("修改套餐操作")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result update(@RequestBody SetmealDTO setmealDTO){
         log.info("修改套餐：{}", setmealDTO);
         setmealService.update(setmealDTO);
@@ -69,6 +73,7 @@ public class SetmealController {
 
     @PostMapping("/status/{status}")
     @ApiOperation("套餐起售、停售操作")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result startORend(@PathVariable Integer status, long id){
         log.info("套餐起售、停售：{}", status);
         setmealService.startORend(status, id);
